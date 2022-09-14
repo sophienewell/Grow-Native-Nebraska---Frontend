b233eea89ecf5bf460742d0a18596d5d9472ae4b
Sophie Newell <sophienewellstudio@gmail.com>
2022-09-13 20:17:37 -0500

    Fixes broken fonts

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,34 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSeedling, faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Menu() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const toggleDropdownOpen = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+  const clickOutside = (e) => {
+    const dropdownbutton = document.getElementById("drop-button");
+    if (dropdownOpen && !dropdownbutton.contains(e.target)) {
+      setDropdownOpen(false);
+    }
+  };
+
+  document.addEventListener("click", (e) => clickOutside(e));
+
   return (
-    <nav className="flex-container bg-green padding-1 font-karla text-white">
-      <FontAwesomeIcon icon={faSeedling} size="xl" />
-      <div>
-        <NavLink className="center link padding-1 text-lg" to="home">
-          Grow Native Nebraska
+    <>
+      <nav className="flex-container bg-green padding-1 font-karla text-white">
+        <FontAwesomeIcon icon={faSeedling} size="xl" />
+        <div>
+          <NavLink className="center link padding-1 text-lg" to="home">
+            Grow Native Nebraska
+          </NavLink>
+        </div>
+        <div className="r-align dropdown">
+          <div
+            id="drop-button"
+            className="drop-button"
+            onClick={toggleDropdownOpen}
+          >
+            <FontAwesomeIcon icon={faBars} size="2xl" />
+          </div>
+        </div>
+      </nav>
+      <div className={"drop-content " + (dropdownOpen ? "show" : "hide")}>
+        <NavLink className="center link-dropdown padding-1" to="home">
+          Home
+        </NavLink>
+        <NavLink className="center link-dropdown padding-1" to="search">
+          Search native plants
+        </NavLink>
+        <NavLink className="center link-dropdown padding-1" to="mailinglist">
+          Mailing List
         </NavLink>
       </div>
-      <div className="r-align dropdown">
-        <div className="drop-button">
-          <FontAwesomeIcon icon={faBars} size="xl" />
-        </div>
-        <div className="drop-content">
-          <NavLink className="center link-dropdown padding-1" to="home">
-            Home
-          </NavLink>
-          <NavLink className="center link-dropdown padding-1" to="search">
-            Search native plants
-          </NavLink>
-          <NavLink className="center link-dropdown padding-1" to="mailinglist">
-            Mailing List
-          </NavLink>
-        </div>
-      </div>
-    </nav>
+    </>
   );
 }
 
